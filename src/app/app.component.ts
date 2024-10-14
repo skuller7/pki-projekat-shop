557aca9b9a16fa2a2e07fd827501a4815574867f
skuller <ukizigic@gmail.com>
2024-10-14 20:57:53 +0200

    Added components, toolbar, Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
