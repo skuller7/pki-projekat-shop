7cd1f40e2c5a0d8ba332c06b4a96313cd81eddbb
skuller <ukizigic@gmail.com>
2024-10-15 21:32:50 +0200

    Pulled data from API, added changes to about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,23 @@
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, JsonPipe, NgIf, NgFor],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  public recommended: any[] | undefined = undefined
 
+  constructor(client: HttpClient) {
+    const url = 'https://private-anon-2bccf8be10-pizzaapp.apiary-mock.com/restaurants/'
+    client.get<any>(url, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).subscribe(rsp => this.recommended = rsp)
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 
@@ -7,7 +7,6 @@ import {MatCardModule} from '@angular/material/card';
   selector: 'app-home',
   standalone: true,
   imports: [MatCardModule, MatButtonModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
