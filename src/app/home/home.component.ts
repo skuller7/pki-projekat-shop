f7d2cd30bd4998d6d3711129e4e00bc56c9120b9
skuller <ukizigic@gmail.com>
2024-10-16 17:57:40 +0200

    Modified home component, Added lists

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import {Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  typesOfPizzaOvens = typesOfPizzaOvens
 }
+
+enum typesOfPizzaOvens {
+
+  option1 = 'Brick Pizza Oven',
+  option2 = "Conveyor Pizza Oven",
+  option3 = "Pizza Deck Oven",
+  option4 = "Convection Pizza Oven",
+  option5 = "Countertop Pizza Oven"
+
+};
+
+//console.log(typesOfPizzaOvens.option1);
